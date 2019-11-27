0abaaf93f5af233a9274af9aab2d05c2b77c4347
Benny <benoitloew@yahoo.fr>
2019-11-27 22:18:11 +0100

    feat: - Order ticket parsing ok

--- src/model/XmlHandling.java
+++ src/model/XmlHandling.java
@@ -2,6 +2,7 @@ package model;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -14,8 +15,11 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
+import java.time.LocalDateTime;
 
 import static java.lang.Thread.sleep;
 
@@ -26,6 +30,9 @@ public class XmlHandling {
     private File xmlFile;
     private Source xmlSource;
     private Document doc;
+    private DocumentBuilderFactory factory;
+    private DocumentBuilder builder;
+    private Element root;
 
     public XmlHandling() {
         xsdFile = new File("xsd.xml");
@@ -49,12 +56,14 @@ public class XmlHandling {
         try {
             validator.validate(xmlSource);
 
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(xmlFile);
-            Element root = doc.getDocumentElement();
+            factory = DocumentBuilderFactory.newInstance();
+            builder = factory.newDocumentBuilder();
+            doc = builder.parse(xmlFile);
+            root = doc.getDocumentElement();
             XPathFactory xpathFactory = XPathFactory.newInstance();
             XPath xpath = xpathFactory.newXPath();
+            processXpath(root);
+
             String exp = "/actions/action/order[2]/clientId";
             String str = xpath.evaluate(exp, root);
 
@@ -63,13 +72,56 @@ public class XmlHandling {
 
 
             System.out.println(xmlSource.getSystemId() + " is valid");
-            System.out.println("client id" + str);
+            //System.out.println("client id" + str);
         } catch (Exception e) {
             System.out.println(xmlSource.getSystemId() + " is NOT valid reason:" + e);
         }
     }
 
-    private void processXpath(NodeList x) {
+    private void processXpath(Element e) {
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+        NodeList orderList;
+        Double idTicket;
+        try {
+            //n = ((NodeList) xpath.evaluate("/actions", e, XPathConstants.NODESET)).item(0);
+            //NodeList x = (NodeList) xpath.evaluate("/", e, XPathConstants.NODESET);
+            idTicket = (Double) xpath.evaluate("/actions/@id", e, XPathConstants.NUMBER);
+            System.out.println("id : " + idTicket);
 
+            //order
+            orderList = (NodeList) xpath.evaluate("//order", e, XPathConstants.NODESET);
+
+            NodeList articleList;
+            Double clientId;
+            Double articleId;
+            OrderTicket orderTicket;
+            for(int i = 0; i < orderList.getLength(); i++) {
+                clientId = (Double) xpath.evaluate("//order["+(i+1)+"]/clientId", e, XPathConstants.NUMBER);
+                articleList = (NodeList) xpath.evaluate("//order["+(i+1)+"]/articleId", e, XPathConstants.NODESET);
+                System.out.println("client : " + clientId.intValue());
+                //loop on articles
+                for(int j = 0; j < articleList.getLength(); j++) {
+                    articleId = (Double) xpath.evaluate("//order["+(i+1)+"]/articleId["+(j+1)+"]", e, XPathConstants.NUMBER);
+                    System.out.println("    article : " + articleId.intValue());
+                    orderTicket = new OrderTicket(idTicket.intValue(), LocalDateTime.now(), clientId.intValue(), articleId.intValue());
+                }
+            }
+
+
+            //supply
+
+
+
+//            for(int i = 0; i < actionList.getLength(); i++) {
+//                tmp = ((NodeList) xpath.evaluate("/action["+i+1+"]", actionList.item(i), XPathConstants.NODESET));
+//                //nature = tmp.item(1).getTextContent();
+//                System.out.println("nature " + tmp.getLength());
+//            }
+//            System.out.println(actionList.getLength() + " actions ");
+
+        } catch (XPathExpressionException ex) {
+            ex.printStackTrace();
+        }
     }
 }
